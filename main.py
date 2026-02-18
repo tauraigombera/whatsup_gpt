b0ad1140b174608c39bbee1e4eb837418c11e2fc
Taurai Gombera <tauraigombera@gmail.com>
2026-02-18 20:27:34 +0200

    registering error handlers

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from twilio.twiml.messaging_response import MessagingResponse
 from gpt_response import response
+from error_handlers import register_error_handlers
 
 app = Flask(__name__)
 
+# Register global error handlers
+register_error_handlers(app)
 
 @app.route("/", methods=["POST"])
 # chatbot logic
